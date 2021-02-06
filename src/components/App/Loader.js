08ddca435c34f036d6fdc42566fe68fb3cd91b05
nazarja <mail.seanmurphy@gmail.com>
2021-02-06 17:39:30 +0000

    Add: added Loader jsx, animation and styles

--- src/components/App/Loader.js
+++ src/components/App/Loader.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { TimelineLite } from 'gsap';
 
 const Loader = () => {
+    const ref = useRef();
+
+    useEffect(() => {
+
+        const tl = new TimelineLite({repeat: -1});
+        tl.to(ref.current, { color: '#3854a0',duration: 1}); // darkblue
+        tl.to(ref.current, { color: '#000',duration: 1}); // black
+        tl.to(ref.current, { color: '#f72d4d',duration: 1}); //red
+        tl.to(ref.current, { color: '#000',duration: 1}); // black
+
+        return () => {
+            tl.kill();
+        }
+    }, []);
+
     return (
-        <div>
-            loading ...
+        <div ref={ref} className="uk-position-center">
+            <div data-uk-spinner="ratio: 3" />
         </div>
     );
 };
