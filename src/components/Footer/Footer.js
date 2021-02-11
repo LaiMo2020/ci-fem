4403f6e6bfe14baffa43383f2772624c8f59727f
nazarja <mail.seanmurphy@gmail.com>
2021-02-11 18:14:38 +0000

    Add: added section titles to footer.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -37,7 +37,7 @@ const Footer = () => {
                 <li key={`devs-${i}`}>
                     <a
                         key={`devs-${i}`}
-                        className="dev-links__link uk-flex uk-flex-center uk-flex-middle uk-flex-right@m"
+                        className="dev-links__link"
                         target="_blank"
                         rel="noopener noreferrer"
                         href={ `https://github.com/${dev[0]}` }
@@ -53,21 +53,24 @@ const Footer = () => {
     return (
         <footer className="uk-position-bottom uk-padding-small">
             <div className="uk-container">
-                <div className="uk-grid">
-                    <div className="uk-width-1-3@m uk-first-column uk-flex uk-flex-middle uk-flex-center uk-flex-left@m">
-                        <ul className="uk-list uk-padding-small quick-links uk-text-center uk-text-left@m">
+                <div className="uk-grid uk-grid-large">
+                    <div className="uk-width-1-4@m">
+                        <h4 className="uk-text-bold">More</h4>
+                        <ul className="quick-links">
                             { renderQuickLinks() }
                         </ul>
                     </div>
-                    <div className="uk-width-1-3@m uk-flex uk-flex-middle uk-flex-center">
-                        <p className="project-desc uk-text-center uk-text-left@m">
-                            This project was a paired programming effort by { renderDevs().length } Developers,
+                    <div className="uk-width-expand@m">
+                        <h4 className="uk-text-bold">About</h4>
+                        <p className="project-description uk-align-center">
+                            This project was a paired programming effort by { renderDevs().length } developers,
                             to create a local space for Code Institute Cohorts to post their solutions to Frontend
                             Mentor challenges and compare amongst their peers.
                         </p>
                     </div>
-                    <div className="uk-width-expand@m uk-flex uk-flex-middle uk-flex-center uk-flex-right@m">
-                        <ul className="uk-list uk-padding-small dev-links uk-text-center uk-text-right@m">
+                    <div className="dev-links uk-width-1-5@m">
+                        <h4 className="uk-text-bold uk-align-right@m">Developed by</h4>
+                        <ul className="uk-align-left@xs  uk-text-left@xs uk-align-right@m uk-text-right@m">
                             { renderDevs() }
                         </ul>
                     </div>
